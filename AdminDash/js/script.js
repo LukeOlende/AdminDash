82815bd36be5a414a16c1b4239478bb8271829cd
LukeOlende <olendeluke2k@gmail.com>
2023-01-10 16:49:22 +0300

    Third Commit

--- AdminDash/js/script.js
+++ AdminDash/js/script.js
@@ -25,7 +25,7 @@ new Chart(ctz, {
     data: {
         labels: ['Jan','Feb','Mar','Apr','May','Jun','Jul'],
         datasets: [{
-            data: [12000,17000,19000,15000,12000,20000,25000],
+            data: [125000,130000,195000,160000,100000,135000,185000],
             borderWidth: 1
         }]
     },
@@ -38,3 +38,36 @@ new Chart(ctz, {
     }
 });
 
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /* Loop through a collection of all HTML elements: */
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+      elmnt = z[i];
+      /*search for elements with a certain atrribute:*/
+      file = elmnt.getAttribute("w3-include-html");
+      if (file) {
+        /* Make an HTTP request using the attribute value as the file name: */
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4) {
+            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+            /* Remove the attribute, and call this function once more: */
+            elmnt.removeAttribute("w3-include-html");
+            includeHTML();
+          }
+        }
+        xhttp.open("GET", file, true);
+        xhttp.send();
+        /* Exit the function: */
+        return;
+      }
+    }
+  }
+
+  $(document).ready(function () {
+    $(".data-table").each(function (_, table) {
+      $(table).DataTable();
+    });
+  });
